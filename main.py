f2e01f2990d262bdf9b5fb5a9c2d31a1599906e9
iUnstable0 <me@iunstable0.com>
2025-08-30 13:58:38 +0700

    tried to move from Flask to FastAPI because cors wont work on Flask idk why i hate python anyways yeh

--- main.py
+++ main.py
@@ -1,157 +1,165 @@
 import os
 from io import BytesIO
 import random
-from flask import Flask, flash, request, redirect, url_for
+import shutil
+# from flask import Flask, flash, request, redirect, url_for, make_response
+from pathlib import Path
+from typing import Union, Annotated, Optional
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
 from werkzeug.utils import secure_filename
 
 from PIL import Image, ImageFont, ImageDraw, ImageFilter, ImageOps, ImageEnhance
 
-UPLOAD_FOLDER = './files'
+UPLOAD_FOLDER = Path("./files")
+UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
+
 ALLOWED_TYPES = ['png', 'jpeg', 'heif', 'heic']
 
 EMOJI_DIR = "./emojis"
 
 fonts = ["./fonts/papyrus.ttf", "./fonts/comic-sans.ttf", "./fonts/fancy.ttf", "./fonts/roboto.ttf"]
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+# app = Flask(__name__)
+app = FastAPI()
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def ghostify(im: Image.Image, ghostpacify, ghostshit) -> Image.Image:
-    im = im.convert("RGBA")
+	im = im.convert("RGBA")
 
-    opacity = max(0.0, min(1.0, ghostpacify))
-    w, h = im.size
+	opacity = max(0.0, min(1.0, ghostpacify))
+	w, h = im.size
 
-    dx = random.randint(-abs(ghostshit), abs(ghostshit))
-    dy = random.randint(-abs(ghostshit), abs(ghostshit))
+	dx = random.randint(-abs(ghostshit), abs(ghostshit))
+	dy = random.randint(-abs(ghostshit), abs(ghostshit))
 
-    ghost = im.copy()
-    ghost.putalpha(int(opacity * 255))
+	ghost = im.copy()
+	ghost.putalpha(int(opacity * 255))
 
-    layer = Image.new("RGBA", (w, h), (0, 0, 0, 0))
+	layer = Image.new("RGBA", (w, h), (0, 0, 0, 0))
 
-    layer.paste(ghost, (dx, dy), ghost)
+	layer.paste(ghost, (dx, dy), ghost)
 
-    return (Image.alpha_composite(im, layer)).convert('RGB')
+	return (Image.alpha_composite(im, layer)).convert('RGB')
 
 
 def deep_fry(im: Image.Image, loops, quality, subsample, posterizebits) -> Image.Image:
-    im = im.convert("RGB")
+	im = im.convert("RGB")
 
-    ## low res ###############
+	## low res ###############
 
-    w, h = im.size
+	w, h = im.size
 
-    im = im.resize((max(1, w // 4), max(1, h // 4)), resample=Image.BOX)
+	im = im.resize((max(1, w // 4), max(1, h // 4)), resample=Image.BOX)
 
-    ## make it look rlly stretched ###############
+	## make it look rlly stretched ###############
 
-    w, h = im.size
+	w, h = im.size
 
-    w_scale = 1.1
-    h_scale = 0.8
+	w_scale = 1.1
+	h_scale = 0.8
 
-    new_size = (max(1, int(w * w_scale)), max(1, int(h * h_scale)))
+	new_size = (max(1, int(w * w_scale)), max(1, int(h * h_scale)))
 
-    im = im.resize(new_size, resample=Image.BOX)
+	im = im.resize(new_size, resample=Image.BOX)
 
-    ## make color tiktok ###############
+	## make color tiktok ###############
 
-    if posterizebits:
-        im = ImageOps.posterize(im, bits=3)
+	if posterizebits:
+		im = ImageOps.posterize(im, bits=3)
 
-    ##############################################
+	##############################################
 
-    # im = ImageEnhance.Color(im).enhance(color)
-    # im = ImageEnhance.Contrast(im).enhance(contrast)
-    # im = ImageEnhance.Brightness(im).enhance(brightness)
+	# im = ImageEnhance.Color(im).enhance(color)
+	# im = ImageEnhance.Contrast(im).enhance(contrast)
+	# im = ImageEnhance.Brightness(im).enhance(brightness)
 
-    for _ in range(loops):
-        buf = BytesIO()
-        im.save(buf, "JPEG", quality=quality, subsampling=subsample, optimize=False, progressive=True)
-        buf.seek(0)
-        im = Image.open(buf).convert("RGB")
+	for _ in range(loops):
+		buf = BytesIO()
+		im.save(buf, "JPEG", quality=quality, subsampling=subsample, optimize=False, progressive=True)
+		buf.seek(0)
+		im = Image.open(buf).convert("RGB")
 
-    return im
+	return im
 
 
 def draw_text(im: Image.Image, text: str, font: int, size: int, xy=(0, 0), fill=(255, 255, 255, 255),
               max_width=None) -> Image.Image:
-    im = im.convert("RGBA")
+	im = im.convert("RGBA")
 
-    draw = ImageDraw.Draw(im)
-    x0, y0 = xy
-    x, y = xy
+	draw = ImageDraw.Draw(im)
+	x0, y0 = xy
+	x, y = xy
 
-    font = fonts[font - 1]
+	font = fonts[font - 1]
 
-    font = ImageFont.truetype(font, size)
+	font = ImageFont.truetype(font, size)
 
-    ascent, descent = font.getmetrics()
-    line_h = int((ascent + descent) * 1.1)
-    emoji_h = int(ascent)
+	ascent, descent = font.getmetrics()
+	line_h = int((ascent + descent) * 1.1)
+	emoji_h = int(ascent)
 
-    max_width = im.size[0]
+	max_width = im.size[0]
 
-    index = 0
-    emojipos = []
+	index = 0
+	emojipos = []
 
-    draw = ImageDraw.Draw(im)
+	draw = ImageDraw.Draw(im)
 
-    for part in text.split(":"):
-        if part.strip() == "":
-            index += 1
-            continue
+	for part in text.split(":"):
+		if part.strip() == "":
+			index += 1
+			continue
 
-        if part + ".png" in os.listdir(EMOJI_DIR):
-            emojipos.append(index)
-            print("Emoji: " + part)
+		if part + ".png" in os.listdir(EMOJI_DIR):
+			emojipos.append(index)
+			print("Emoji: " + part)
 
-            emoji = Image.open(os.path.join(EMOJI_DIR, part + ".png")).convert("RGBA")
-            eh = emoji.size[1]
-            ew = emoji.size[0]
+			emoji = Image.open(os.path.join(EMOJI_DIR, part + ".png")).convert("RGBA")
+			eh = emoji.size[1]
+			ew = emoji.size[0]
 
-            new_w = max(1, int(ew * (emoji_h / eh)))
-            new_h = max(1, int(emoji_h))
+			new_w = max(1, int(ew * (emoji_h / eh)))
+			new_h = max(1, int(emoji_h))
 
-            emoji = emoji.resize((new_w, new_h), resample=Image.LANCZOS)
+			emoji = emoji.resize((new_w, new_h), resample=Image.LANCZOS)
 
-            # im.paste(emoji, (x, y), emoji)
-            # im.paste(emoji, (int(x), int(y)), emoji)
-            im.paste(emoji, (int(x), int(y)), emoji)
-            x += emoji.size[0]
-        else:
-            length = int(draw.textlength(part, font=font))
-            print(f"Text: {part} length={length} x={x} y={y}")
+			# im.paste(emoji, (x, y), emoji)
+			# im.paste(emoji, (int(x), int(y)), emoji)
+			im.paste(emoji, (int(x), int(y)), emoji)
+			x += emoji.size[0]
+		else:
+			length = int(draw.textlength(part, font=font))
+			print(f"Text: {part} length={length} x={x} y={y}")
 
-            if length > max_width:
-                for char in part:
-                    print(char)
-                    char_length = int(draw.textlength(char, font=font))
-                    if max_width and x + char_length > x0 + max_width:
-                        print(f"Character too long: {char} x={x0} y={y}")
-                        x = x0
-                        y += line_h
-                    # draw.text((x, y), char, fill=fill, font=font)
-                    draw.text((int(x), int(y)), char, fill=fill, font=font)
-                    x += char_length
-            else:
-                # draw.text((x, y), part, fill=fill, font=font)
-                draw.text((int(x), int(y)), part, fill=fill, font=font)  # be safe
-                x += draw.textlength(part, font=font)
+			if length > max_width:
+				for char in part:
+					print(char)
+					char_length = int(draw.textlength(char, font=font))
+					if max_width and x + char_length > x0 + max_width:
+						print(f"Character too long: {char} x={x0} y={y}")
+						x = x0
+						y += line_h
+					# draw.text((x, y), char, fill=fill, font=font)
+					draw.text((int(x), int(y)), char, fill=fill, font=font)
+					x += char_length
+			else:
+				# draw.text((x, y), part, fill=fill, font=font)
+				draw.text((int(x), int(y)), part, fill=fill, font=font)  # be safe
+				x += draw.textlength(part, font=font)
 
-            print(part)
+			print(part)
 
-        index += 1
+		index += 1
 
-    return im.convert("RGB")
+	return im.convert("RGB")
 
 
-@app.route("/")
+@app.get("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+	return "<p>Hello, World!</p>"
 
 
 # def allowed_file(filename: str) -> bool:
@@ -160,69 +168,106 @@ def hello_world():
 #     ext = filename.rsplit('.', 1)[1].lower()
 #     return ext in ALLOWED_TYPES
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return {"error": "No file part in request (expect key 'file')"}, 400
+class FormData(BaseModel):
+	file: UploadFile
+	quality: int = 20
+	loops: int = 3
+	subsample: int = 2
+	posterizebits: bool = True
+	brightness: float = 1.0
+	contrast: float = 1.0
+	ghost: bool = True
+	ghostpacify: float = 0.5
+	ghostshit: int = 10
+	font: int = 1
+	r: int = 255
+	g: int = 255
+	b: int = 255
+	alpha: int = 255
+	message: str = "Hello,  My Goat ❤️"
 
-    file = request.files['file']
 
-    if file.filename == '':
-        return {"error": "No selected file"}, 400
+@app.post('/upload')
+def upload_file(request: Annotated[FormData, Form()]):
+	# print("GOAT GOAT GOAT LLOOG" + request.file)
+	if not request.file:
+		return {"error": "No file part in request (expect key 'file')"}, 400
 
-    mimetype = file.content_type
+	file = request.file
 
-    print(mimetype)
+	if file.filename == '':
+		return {"error": "No selected file"}, 400
 
-    if mimetype.split('/')[1] not in ALLOWED_TYPES:
-        return {"error": f"Unsupported file type. Allowed: {', '.join(ALLOWED_TYPES)}"}, 400
-    else:
-        print("supported")
+	mimetype = file.content_type
 
-    quality = request.form.get('quality', default=20, type=int)
-    loops = request.form.get('loops', default=3, type=int)
-    subsample = request.form.get('subsample', default=2, type=int)
+	print(mimetype)
 
-    posterizebits = request.form.get('posterizebits', default="true", type=str).lower() == "true"
+	if mimetype.split('/')[1] not in ALLOWED_TYPES:
+		return {"error": f"Unsupported file type. Allowed: {', '.join(ALLOWED_TYPES)}"}, 400
+	else:
+		print("supported")
 
-    brightness = request.form.get('brightness', default=1, type=float)
-    contrast = request.form.get('contrast', default=1, type=float)
+	quality = request.quality
+	loops = request.loops
+	subsample = request.subsample
+	posterizebits = request.posterizebits
+	brightness = request.brightness
+	contrast = request.contrast
+	ghost = request.ghost
+	ghostpacify = request.ghostpacify
+	ghostshit = request.ghostshit
+	font = request.font
+	r = request.r
+	g = request.g
+	b = request.b
+	alpha = request.alpha
+	message = request.message
 
-    ghost = request.form.get('ghost', default="true", type=str).lower() == "true"
-    ghostpacify = request.form.get('ghostpacify', default=0.5, type=float)
-    ghostshit = request.form.get('ghostshit', default=10, type=int)
+	# quality = request.form.get('quality', default=20, type=int)
+	# loops = request.form.get('loops', default=3, type=int)
+	# subsample = request.form.get('subsample', default=2, type=int)
+	#
+	# posterizebits = request.form.get('posterizebits', default="true", type=str).lower() == "true"
+	#
+	# brightness = request.form.get('brightness', default=1, type=float)
+	# contrast = request.form.get('contrast', default=1, type=float)
+	#
+	# ghost = request.form.get('ghost', default="true", type=str).lower() == "true"
+	# ghostpacify = request.form.get('ghostpacify', default=0.5, type=float)
+	# ghostshit = request.form.get('ghostshit', default=10, type=int)
+	#
+	# font = request.form.get('font', default=1, type=int)
+	#
+	# r = int(request.form.get('r', default=255, type=int))
+	# g = int(request.form.get('g', default=255, type=int))
+	# b = int(request.form.get('b', default=255, type=int))
+	# alpha = int(request.form.get('alpha', default=255, type=int))
+	#
+	# message = request.form.get('message', default="Hello,  My Goat ❤️", type=str)
 
-    font = request.form.get('font', default=1, type=int)
+	print(quality, loops, subsample, posterizebits)
+	# return {"error": "Testing"}, 400
 
-    r = int(request.form.get('r', default=255, type=int))
-    g = int(request.form.get('g', default=255, type=int))
-    b = int(request.form.get('b', default=255, type=int))
-    alpha = int(request.form.get('alpha', default=255, type=int))
+	filename = secure_filename(file.filename)
 
-    message = request.form.get('message', default="Hello,  My Goat ❤️", type=str)
+	save_path = os.path.join(UPLOAD_FOLDER, filename)
+	file.save(save_path)
 
-    print(quality, loops, subsample, posterizebits)
-    # return {"error": "Testing"}, 400
+	with Image.open(save_path) as im:
+		im = ImageEnhance.Brightness(im).enhance(brightness)
+		im = ImageEnhance.Contrast(im).enhance(contrast)
 
-    filename = secure_filename(file.filename)
-    save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(save_path)
+		# font = ImageFont.truetype("./fonts/papyrus.ttf", im.size[1] // 8)
 
-    with Image.open(save_path) as im:
-        im = ImageEnhance.Brightness(im).enhance(brightness)
-        im = ImageEnhance.Contrast(im).enhance(contrast)
+		# draw = ImageDraw.Draw(im)
+		# draw.text((1,1), "Hello,  My Goat", font=font, fill=(r,g,b, alpha))
 
-        # font = ImageFont.truetype("./fonts/papyrus.ttf", im.size[1] // 8)
+		im = draw_text(im, message, font, im.size[1] // 8, fill=(r, g, b, alpha))
+		im = deep_fry(im, loops=loops, quality=quality, subsample=subsample, posterizebits=posterizebits)
 
-        # draw = ImageDraw.Draw(im)
-        # draw.text((1,1), "Hello,  My Goat", font=font, fill=(r,g,b, alpha))
+		if ghost:
+			im = ghostify(im, ghostpacify=ghostpacify, ghostshit=ghostshit)
 
-        im = draw_text(im, message, font, im.size[1] // 8, fill=(r, g, b, alpha))
-        im = deep_fry(im, loops=loops, quality=quality, subsample=subsample, posterizebits=posterizebits)
+		im.save(save_path, "JPEG")
 
-        if ghost:
-            im = ghostify(im, ghostpacify=ghostpacify, ghostshit=ghostshit)
-
-        im.save(save_path, "JPEG")
-
-    return {"ok": True, "filename": filename, "path": save_path}, 201
+	return {"ok": True, "filename": filename, "path": save_path}, 201
